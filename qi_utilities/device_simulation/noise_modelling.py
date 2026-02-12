941d2227ffbc06dd77d24f5039ad45ee66365959
Marios Samiotis <mariossam95@gmail.com>
2026-02-12 17:18:42 +0100

    Checkpoint commit

--- qi_utilities/device_simulation/noise_modelling.py
+++ qi_utilities/device_simulation/noise_modelling.py
@@ -5,6 +5,7 @@ import ast
 import numpy as np
 
 T1_FUDGE_FACTOR = 0.6
+T2_FUDGE_FACTOR = 0.6
 
 def relaxation_channel(temperature, qubit_freq, qubit_T1, time_interval):
     kraus_op = []
@@ -61,39 +62,26 @@ def create_noise_model(processor_specs: dict):
         
         decay_prob = 1 / (1 + np.exp( (-Planck * processor_specs['Qubits'][qubit_list[qubit_idx]]['Frequency [Hz]']) / (Boltzmann * processor_specs['Base temperature [K]']) ))
         relaxation_dephasing_delay = noise.thermal_relaxation_error(t1 = T1_FUDGE_FACTOR * processor_specs['Qubits'][qubit_list[qubit_idx]]['T1 [s]'] * 1e6,
-                                                                    t2 = processor_specs['Qubits'][qubit_list[qubit_idx]]['T2 [s]'] * 1e6,
+                                                                    t2 = T2_FUDGE_FACTOR * processor_specs['Qubits'][qubit_list[qubit_idx]]['T2 [s]'] * 1e6,
                                                                     time = 1,
                                                                     excited_state_population = 1 - decay_prob)
-        relaxation_noise_delay = noise.kraus_error(relaxation_channel(temperature = processor_specs['Base temperature [K]'],
-                                                                      qubit_freq = processor_specs['Qubits'][qubit_list[qubit_idx]]['Frequency [Hz]'],
-                                                                      qubit_T1 = processor_specs['Qubits'][qubit_list[qubit_idx]]['T1 [s]'],
-                                                                      time_interval = processor_specs['Delay duration [s]']))
-        dephasing_noise_delay = noise.kraus_error(pure_dephasing_channel(qubit_T1 = processor_specs['Qubits'][qubit_list[qubit_idx]]['T1 [s]'],
-                                                                         qubit_T2 = processor_specs['Qubits'][qubit_list[qubit_idx]]['T2 [s]'],
-                                                                         time_interval = processor_specs['Delay duration [s]']))
+        relaxation_dephasing_measure = noise.thermal_relaxation_error(t1 = T1_FUDGE_FACTOR * processor_specs['Qubits'][qubit_list[qubit_idx]]['T1 [s]'] * 1e6,
+                                                                      t2 = T2_FUDGE_FACTOR * processor_specs['Qubits'][qubit_list[qubit_idx]]['T2 [s]'] * 1e6,
+                                                                      time = processor_specs['Measurement duration [s]'] * 1e6,
+                                                                      excited_state_population = 1 - decay_prob)
         
         epsilon_cl = 1 - (1 - processor_specs['Qubits'][qubit_list[qubit_idx]]['RB error'])**n_g
         depolarizing_noise = noise.kraus_error(depolarization_channel(num_qubits = 1,
                                                                       epsilon_cl = epsilon_cl))
         
-        relaxation_noise_measurement = noise.kraus_error(relaxation_channel(temperature = processor_specs['Base temperature [K]'],
-                                                                            qubit_freq = processor_specs['Qubits'][qubit_list[qubit_idx]]['Frequency [Hz]'],
-                                                                            qubit_T1 = T1_FUDGE_FACTOR * processor_specs['Qubits'][qubit_list[qubit_idx]]['T1 [s]'],
-                                                                            time_interval = processor_specs['Measurement duration [s]']))
-        dephasing_noise_measurement = noise.kraus_error(pure_dephasing_channel(qubit_T1 = T1_FUDGE_FACTOR * processor_specs['Qubits'][qubit_list[qubit_idx]]['T1 [s]'],
-                                                                               qubit_T2 = processor_specs['Qubits'][qubit_list[qubit_idx]]['T2 [s]'],
-                                                                               time_interval = processor_specs['Measurement duration [s]']))
         readout_error = noise.ReadoutError([[1 - processor_specs['Qubits'][qubit_list[qubit_idx]]['SSRO']['p1given0'],
                                              processor_specs['Qubits'][qubit_list[qubit_idx]]['SSRO']['p1given0']],
                                              [processor_specs['Qubits'][qubit_list[qubit_idx]]['SSRO']['p0given1'],
                                               1 - processor_specs['Qubits'][qubit_list[qubit_idx]]['SSRO']['p0given1']]])
         
-        simulator_noise_model.add_quantum_error(relaxation_noise_delay, ['delay'], [qubit_idx], warnings=False)
         simulator_noise_model.add_quantum_error(relaxation_dephasing_delay, ['delay'], [qubit_idx], warnings=False)
-        simulator_noise_model.add_quantum_error(dephasing_noise_delay, ['delay'], [qubit_idx], warnings=False)
+        simulator_noise_model.add_quantum_error(relaxation_dephasing_measure, ['measure'], [qubit_idx], warnings=False)
         simulator_noise_model.add_quantum_error(depolarizing_noise, ['id', 's', 'sdg', 't', 'tdg', 'x', 'rx', 'y', 'ry', 'z'], [qubit_idx], warnings=False)
-        simulator_noise_model.add_quantum_error(relaxation_noise_measurement, ['measure'], [qubit_idx], warnings=False)
-        simulator_noise_model.add_quantum_error(dephasing_noise_measurement, ['measure'], [qubit_idx], warnings=False)
         simulator_noise_model.add_readout_error(readout_error, [qubit_idx])
         
     for CZ_idx in range(len(CZ_list)):
@@ -102,7 +90,7 @@ def create_noise_model(processor_specs: dict):
         depolarizing_noise_CZ = noise.kraus_error(depolarization_channel(num_qubits = 2,
                                                                          epsilon_cl = epsilon_cl))
         
-        # simulator_noise_model.add_quantum_error(depolarizing_noise_CZ, ['cz'], ast.literal_eval(CZ_list[CZ_idx]), warnings=False)
+        simulator_noise_model.add_quantum_error(depolarizing_noise_CZ, ['cz'], ast.literal_eval(CZ_list[CZ_idx]), warnings=False)
         # simulator_noise_model.add_quantum_error(depolarizing_noise_CZ, ['cz'], ast.literal_eval(CZ_list[CZ_idx])[::-1], warnings=False) # should this be here??
         # test with simple Bell state with CZ in both directions
 
